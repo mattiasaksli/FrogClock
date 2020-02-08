8d23455280abb22f8e69fae7d80b5614ca2ac7c4
Tanel Marran <marran.tanel@gmail.com>
2020-02-08 14:14:20 +0200

    Basic code for the app

---
+++ lib/main.dart
@@ -0,0 +1,358 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
+import 'package:bluetooth/bluetooth.dart';
+import 'dart:io' as io;
+// For changing the language
+//import 'package:flutter_localizations/flutter_localizations.dart';
+//import 'package:flutter_cupertino_localizations/flutter_cupertino_localizations.dart';
+
+const appName = 'FrogAlarm';
+const PrimaryColor = Colors.green;
+
+FlutterBlue flutterBlue = FlutterBlue.instance;
+
+void main() {
+  FlutterBlue flutterBlue = FlutterBlue.instance;
+
+  runApp(MaterialApp(
+      title: appName,
+      home: MyHomePage(),
+      theme: ThemeData(
+        primaryColor: PrimaryColor,
+      ),
+      supportedLocales: [
+        const Locale('en'), // English
+      ],
+    ));
+}
+
+void bluetoothSearch() {
+  /// Start scanning
+  var scanSubscription = flutterBlue.scan().listen((scanResult) {
+    var deviceConnection = flutterBlue.connect(scanResult.device).listen((s) {
+      if(s == BluetoothDeviceState.connected) {
+        io.stderr.writeln('print me');
+      }
+    });
+
+    deviceConnection.cancel();
+  });
+
+  /// Stop scanning
+  scanSubscription.cancel();
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  bool isSwitched = false;
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+          appBar: AppBar(
+            /*bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.directions_transit)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),*/
+          ),
+          body: Center(
+            child: Container(
+              width: 200,
+              child: Column(
+                children: <Widget>[
+                  DateTimeForm(),
+                  Switch(
+                    value: isSwitched,
+                    onChanged: (value) {
+                      setState(() {
+                        isSwitched = value;
+                        print(isSwitched);
+                      });
+                    },
+                    activeTrackColor: Colors.lightGreenAccent,
+                    activeColor: Colors.green,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      bluetoothSearch();
+                    },
+                    child: const Text('Bluetooth search', style: TextStyle(fontSize: 20)),
+                  ),
+                ],
+              ),
+            ),
+          )),
+    );
+  }
+}
+
+class DateTimeForm extends StatefulWidget {
+  @override
+  _DateTimeFormState createState() => _DateTimeFormState();
+}
+
+class _DateTimeFormState extends State<DateTimeForm> {
+  final formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return BasicTimeField();
+  }
+}
+
+class BasicDateField extends StatelessWidget {
+  final format = DateFormat("yyyy-MM-dd");
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Text('Basic date field (${format.pattern})'),
+      DateTimeField(
+        format: format,
+        onShowPicker: (context, currentValue) {
+          return showDatePicker(
+              context: context,
+              firstDate: DateTime(1900),
+              initialDate: currentValue ?? DateTime.now(),
+              lastDate: DateTime(2100));
+        },
+      ),
+    ]);
+  }
+}
+
+class BasicTimeField extends StatelessWidget {
+  final format = DateFormat("hh:mm a");
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Text('Basic time field (${format.pattern})'),
+          DateTimeField(
+            format: format,
+            resetIcon: null,
+            onShowPicker: (context, currentValue) async {
+              final time = await showTimePicker(
+                context: context,
+                initialTime:
+                    TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+              );
+              return DateTimeField.convert(time);
+            },
+          ),
+        ]);
+  }
+}
+
+class BasicDateTimeField extends StatelessWidget {
+  final format = DateFormat("yyyy-MM-dd HH:mm");
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Text('Basic date & time field (${format.pattern})'),
+      DateTimeField(
+        format: format,
+        onShowPicker: (context, currentValue) async {
+          final date = await showDatePicker(
+              context: context,
+              firstDate: DateTime(1900),
+              initialDate: currentValue ?? DateTime.now(),
+              lastDate: DateTime(2100));
+          if (date != null) {
+            final time = await showTimePicker(
+              context: context,
+              initialTime:
+                  TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+            );
+            return DateTimeField.combine(date, time);
+          } else {
+            return currentValue;
+          }
+        },
+      ),
+    ]);
+  }
+}
+
+class IosStylePickers extends StatefulWidget {
+  @override
+  _IosStylePickersState createState() => _IosStylePickersState();
+}
+
+class _IosStylePickersState extends State<IosStylePickers> {
+  final format = DateFormat("yyyy-MM-dd HH:mm");
+  DateTime value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Text('iOS style pickers (${format.pattern})'),
+      DateTimeField(
+        initialValue: value,
+        format: format,
+        onShowPicker: (context, currentValue) async {
+          await showCupertinoModalPopup(
+              context: context,
+              builder: (context) {
+                return CupertinoDatePicker(
+                  onDateTimeChanged: (DateTime date) {
+                    value = date;
+                  },
+                );
+              });
+          setState(() {});
+          return value;
+        },
+      ),
+    ]);
+  }
+}
+
+class ComplexDateTimeField extends StatefulWidget {
+  @override
+  _ComplexDateTimeFieldState createState() => _ComplexDateTimeFieldState();
+}
+
+class _ComplexDateTimeFieldState extends State<ComplexDateTimeField> {
+  final format = DateFormat("yyyy-MM-dd HH:mm");
+  final initialValue = DateTime.now();
+
+  bool autoValidate = false;
+  bool readOnly = true;
+  bool showResetIcon = true;
+  DateTime value = DateTime.now();
+  int changedCount = 0;
+  int savedCount = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Text('Complex date & time field (${format.pattern})'),
+      DateTimeField(
+        format: format,
+        onShowPicker: (context, currentValue) async {
+          final date = await showDatePicker(
+              context: context,
+              firstDate: DateTime(1900),
+              initialDate: currentValue ?? DateTime.now(),
+              lastDate: DateTime(2100));
+          if (date != null) {
+            final time = await showTimePicker(
+              context: context,
+              initialTime:
+                  TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+            );
+            return DateTimeField.combine(date, time);
+          } else {
+            return currentValue;
+          }
+        },
+        autovalidate: autoValidate,
+        validator: (date) => date == null ? 'Invalid date' : null,
+        initialValue: initialValue,
+        onChanged: (date) => setState(() {
+          value = date;
+          changedCount++;
+        }),
+        onSaved: (date) => setState(() {
+          value = date;
+          savedCount++;
+        }),
+        resetIcon: showResetIcon ? Icon(Icons.delete) : null,
+        readOnly: readOnly,
+        decoration: InputDecoration(
+            helperText: 'Changed: $changedCount, Saved: $savedCount, $value'),
+      ),
+      CheckboxListTile(
+        title: Text('autoValidate'),
+        value: autoValidate,
+        onChanged: (value) => setState(() => autoValidate = value),
+      ),
+      CheckboxListTile(
+        title: Text('readOnly'),
+        value: readOnly,
+        onChanged: (value) => setState(() => readOnly = value),
+      ),
+      CheckboxListTile(
+        title: Text('showResetIcon'),
+        value: showResetIcon,
+        onChanged: (value) => setState(() => showResetIcon = value),
+      ),
+    ]);
+  }
+}
+
+class Clock24Example extends StatelessWidget {
+  final format = DateFormat("HH:mm");
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Text('24 hour clock'),
+      DateTimeField(
+        format: format,
+        onShowPicker: (context, currentValue) async {
+          final time = await showTimePicker(
+            context: context,
+            initialTime: TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+            builder: (context, child) => MediaQuery(
+                data: MediaQuery.of(context)
+                    .copyWith(alwaysUse24HourFormat: true),
+                child: child),
+          );
+          return DateTimeField.convert(time);
+        },
+      ),
+    ]);
+  }
+}
+
+class LocaleExample extends StatelessWidget {
+  final format = DateFormat("yyyy-MM-dd HH:mm");
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: <Widget>[
+      Text('Changing the pickers\' language'),
+      DateTimeField(
+        format: format,
+        onShowPicker: (context, currentValue) async {
+          final date = await showDatePicker(
+            context: context,
+            firstDate: DateTime(1900),
+            initialDate: DateTime.now(),
+            lastDate: DateTime(2100),
+            builder: (context, child) => Localizations.override(
+              context: context,
+              locale: Locale('zh'),
+              child: child,
+            ),
+          );
+          if (date != null) {
+            final time = await showTimePicker(
+              context: context,
+              initialTime:
+                  TimeOfDay.fromDateTime(currentValue ?? DateTime.now()),
+              builder: (context, child) => Localizations.override(
+                context: context,
+                locale: Locale('zh'),
+                child: child,
+              ),
+            );
+            return DateTimeField.combine(date, time);
+          } else {
+            return currentValue;
+          }
+        },
+      ),
+    ]);
+  }
+}
